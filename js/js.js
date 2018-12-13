018bedb8123e796d36bba61d47ddfc471e07562f
jsjlx <1242544654@qq.com>
2018-12-13 11:31:28 +0800

    web update 0.1 lx

---
+++ js/js.js
@@ -0,0 +1,160 @@
+// window.onresize = add()
+// function add(){
+//     var wid= window.innerWidth;
+//     console.log(wid);
+    
+//     if(wid <= 992){
+//         document.getElementById("delete").classList.remove("container");
+//     } else if( wid>992){
+//         document.getElementById("delete").className+="container";
+//     }
+// }
+(function($) {
+
+    $.fn.menumaker = function(options) {
+        
+        var cssmenu = $(this), settings = $.extend({
+          title: "Menu",
+          format: "dropdown",
+          sticky: false
+        }, options);
+  
+        return this.each(function() {
+          cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+          $(this).find("#menu-button").on('click', function(){
+            $(this).toggleClass('menu-opened');
+            var mainmenu = $(this).next('ul');
+            if (mainmenu.hasClass('open')) { 
+              mainmenu.hide().removeClass('open');
+            }
+            else {
+              mainmenu.show().addClass('open');
+              if (settings.format === "dropdown") {
+                mainmenu.find('ul').show();
+              }
+            }
+          });
+  
+          cssmenu.find('li ul').parent().addClass('has-sub');
+  
+          multiTg = function() {
+            cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+            cssmenu.find('.submenu-button').on('click', function() {
+              $(this).toggleClass('submenu-opened');
+              if ($(this).siblings('ul').hasClass('open')) {
+                $(this).siblings('ul').removeClass('open').hide();
+              }
+              else {
+                $(this).siblings('ul').addClass('open').show();
+              }
+            });
+          };
+  
+          if (settings.format === 'multitoggle') multiTg();
+          else cssmenu.addClass('dropdown');
+  
+          if (settings.sticky === true) cssmenu.css('position', 'fixed');
+  
+          resizeFix = function() {
+            if ($( window ).width() > 768) {
+              cssmenu.find('ul').show();
+            }
+  
+            if ($(window).width() <= 768) {
+              cssmenu.find('ul').hide().removeClass('open');
+            }
+          };
+          resizeFix();
+          return $(window).on('resize', resizeFix);
+  
+        });
+    };
+  })(jQuery);
+  
+  (function($){
+  $(document).ready(function(){
+  
+  $(document).ready(function() {
+    $("#cssmenu").menumaker({
+      title: "计算机工程学院",
+      format: "multitoggle"
+    });
+  
+    // $("#cssmenu").prepend("<div id='menu-line'></div>");
+  
+  var foundActive = false, activeElement, linePosition = 0, menuLine = $("#cssmenu #menu-line"), lineWidth, defaultPosition, defaultWidth;
+  
+  $("#cssmenu > ul > li").each(function() {
+    if ($(this).hasClass('active')) {
+      activeElement = $(this);
+      foundActive = true;
+    }
+  });
+  
+  if (foundActive === false) {
+    activeElement = $("#cssmenu > ul > li").first();
+  }
+  
+  defaultWidth = lineWidth = activeElement.width();
+  
+  defaultPosition = linePosition = activeElement.position().left;
+  
+  menuLine.css("width", lineWidth);
+  menuLine.css("left", linePosition);
+  
+  $("#cssmenu > ul > li").hover(function() {
+    activeElement = $(this);
+    lineWidth = activeElement.width();
+    // linePosition = activeElement.position().left;
+    menuLine.css("width", lineWidth);
+    menuLine.css("left", linePosition);
+  }, 
+  function() {
+    menuLine.css("left", defaultPosition);
+    menuLine.css("width", defaultWidth);
+  });
+  
+  });
+  
+  
+  });
+  })(jQuery);
+
+
+window.onload = function() {
+   
+    var mySwiper = new Swiper ('.swiper-container', {
+      direction: 'horizontal', // 垂直切换选项
+      loop: true, // 循环模式选项
+      //鼠标移上去变小手
+      grabCursor : true,
+      //切换速度
+      speed: 2000,
+      autoplay: {
+        delay:2000
+      },
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      
+      // 如果需要滚动条
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      //强制加载所有图片
+      // preloadImages:false,
+    })        
+}
+
+  
+
+
+
+
